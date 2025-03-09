a85c9f42d84e15b83c2326d50ffb6c82dc65f82c
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2025-03-09 13:37:01 +0100

    update modal_form.js

--- Front End/src/modal_form.js
+++ Front End/src/modal_form.js
@@ -14,3 +14,32 @@ checkbox.addEventListener('change', e => {
         checkbox.value = 0
     }
 });
+
+function sendFormData(event) {
+    event.preventDefault();
+    const parts = ModalForm.inputDate.value.split('.');
+    const formData = {
+        transactionType: ModalForm.transactionSelection.value,
+        name: ModalForm.inputTitle.value,
+        category: ModalForm.categorySelection.value,
+        date: `${parts[2]}-${parts[1]}-${parts[0]}`,
+        value: ModalForm.inputAmount.value.replace(',', '.'),
+        taxational_relevant: checkbox.value
+    };
+    const jsonData = JSON.stringify(formData);
+
+    fetch('http://localhost:4444/v1/newTransaction', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
