2f9cb7bdb79af54ca5cb3f36086a65d7db8f8dcf
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2025-03-09 17:32:09 +0100

    update backend

--- Backend/projectbalance.js
+++ Backend/projectbalance.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('~/apps/ProjectBalance/Project-Balance-2/Frontend/'));
+app.use(express.static(path.join(__dirname, 'Frontend')));
 app.use(express.json())
 
 app.listen(4444, 'localhost', () => {
