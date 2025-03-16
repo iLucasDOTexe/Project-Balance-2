76edd1877d14949c982013ffc406949fdecd7c1c
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2025-03-16 16:39:06 +0100

    add delete functionality to button

--- Frontend/src/transaction_table.js
+++ Frontend/src/transaction_table.js
@@ -16,7 +16,7 @@ function loadTransactions() {
                         <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenuButton_${index}" data-bs-toggle="dropdown" aria-expanded="false"></button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton_${index}">
                             <li><a class="dropdown-item" href="#">Bearbeiten</a></li>
-                            <li><a class="dropdown-item" href="#">Löschen</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="deleteTransaction(${tx.id}, '${tx.transactionType}')">Löschen</a></li>
                         </ul>
                     </div>
                 </td>`;
