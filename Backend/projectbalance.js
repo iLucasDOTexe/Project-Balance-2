83be16d3131ea9076d36aba6ff923d5fa7a0d703
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2025-03-16 10:43:13 +0100

    create tables

--- Backend/projectbalance.js
+++ Backend/projectbalance.js
@@ -9,6 +9,28 @@ let db = new sqlite3.Database(dbPath, (err) => {
         return console.error("Connection error with database: ", err.message);
     }
     console.log("Database connected");
+
+    db.serialize(() => {
+        db.run('CREATE TABLE IF NOT EXISTS Income (Transaction_Number INTEGER PRIMATY KEY ANTOINCREMENT, Transaction_Name TEXT NOT NULL, Transaction_Value REAL NOT NULL, Transaction_Date TEXT NOT NULL)', (err) => {
+            if (err) {
+                return console.error("Error during creation of Income Table: ", err.message);
+            }
+            console.log("Table Income created or already existing");
+        });
+        db.run('CREATE TABLE IF NOT EXISTS Savings (Transaction_Number INTEGER PRIMATY KEY ANTOINCREMENT, Transaction_Name TEXT NOT NULL, Transaction_Category TEXT, Transaction_Value REAL NOT NULL, Transaction_Date TEXT NOT NULL)', (err) => {
+            if (err) {
+                return console.error("Error during creation of Savings Table: ", err.message);
+            }
+            console.log("Table Savings created or already existing");
+        });
+        db.run('CREATE TABLE IF NOT EXISTS Spendings (Transaction_Number INTEGER PRIMATY KEY ANTOINCREMENT, Transaction_Name TEXT NOT NULL, Transaction_Value REAL NOT NULL, Transaction_Date TEXT NOT NULL)', (err) => {
+            if (err) {
+                return console.error("Error during creation of Spendings Table: ", err.message);
+            }
+            console.log("Table Spendings created or already existing");
+        });
+    });
+
 });
 
 app.use(express.static('Frontend'));
