a145a9f2e9000aa09dff744f31099bf87ba0f18e
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2025-03-16 15:38:38 +0100

    change transaction table button layout

--- Frontend/src/transaction_table.js
+++ Frontend/src/transaction_table.js
@@ -13,9 +13,7 @@ function loadTransactions() {
                 <td>${tx.value > 0 ? '+' : ''}${tx.value}€</td>
                 <td>
                     <div class="dropdown">
-                        <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenuButton_${index}" data-bs-toggle="dropdown" aria-expanded="false">
-                            ⋮
-                        </button>
+                        <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenuButton_${index}" data-bs-toggle="dropdown" aria-expanded="false"></button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton_${index}">
                             <li><a class="dropdown-item" href="#">Bearbeiten</a></li>
                             <li><a class="dropdown-item" href="#">Löschen</a></li>
